067e3b74c668275b9318ff4ed1b9dd0ba35ae407
Jorge Luiz <defiant.jr@outlook.com>
2025-11-03 15:05:24 -0300

    retorno 1

--- .vite/deps_temp_e1e7a511/browser-ponyfill-X3ESHEGI.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_browser_ponyfill
-} from "./chunk-PDRPOJTI.js";
-import "./chunk-ROME4SDB.js";
-export default require_browser_ponyfill();
-//# sourceMappingURL=browser-ponyfill-X3ESHEGI.js.map
